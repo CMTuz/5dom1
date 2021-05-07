752d9d1705f4a87e8f6d61f12006094039cceb5f
CMTuz <83449012+CMTuz@users.noreply.github.com>
2021-05-07 13:24:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+var x = prompt('введите время');
+x = Number(x);
+switch (x) {
+        case 1:
+        alert(' 01:00 ночи');
+        break;
+         case 2:
+        alert('02:00 ночи ');
+        break;
+         case 3:
+        alert('03:00 ночи');
+        break;
+        case 4:
+        alert('04:00 ночи');
+        break;
+        case 5:
+        alert('05:00 утра');
+        break; 
+        case 6:
+        alert('06:00 утра');
+        break; 
+        case 7:
+        alert('07:00 утра');
+        break;
+        case 8:
+        alert('08:00 утра');
+        break;
+        case 9:
+        alert('09:00 утра');
+        break;
+        case 10:
+        alert('10:00 утра');
+        break;
+        case 11:
+        alert('11:00 утра');
+        break;
+        case 12:
+        alert('12:00 дня');
+        break;
+        case 13:
+        alert('13:00 дня');
+        break;
+        case 14:
+        alert('14:00 дня');
+        break;
+        case 15:
+        alert('15:00 дня');
+        break;
+        case 16:
+        alert('16:00 дня');
+        break;
+        case 17:
+        alert('17:00 дня');
+        break;
+        case 18:
+        alert('18:00 дня');
+        break;
+        case 19:
+        alert('19:00 дня');
+        break;
+        case 20:
+        alert('20:00 вечера');
+        break;
+        case 21:
+        alert('21:00 вечера');
+        break;
+        case 22:
+        alert('22:00 вечера');
+        break;
+        case 23:
+        alert('23:00 вечера');
+        break;
+       
+
+    default:
+        alert('ошибка')
+        break;
+
+    
+}
